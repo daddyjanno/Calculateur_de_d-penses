a0cca813311c67a0ac8d6505040b61bdbad51f84
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-27 12:19:53 +0100

    Details component

---
+++ Calculateur/src/components/Detail/Detail.tsx
@@ -0,0 +1,21 @@
+function Detail() {
+    return (
+        <>
+            <h2 className="text-center">Operation details</h2>
+            <table className="table table-sm table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>amount</th>
+                        <th>actions</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+            <hr />
+        </>
+    )
+}
+
+export default Detail
