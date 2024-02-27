bc513bc7759dc69a08d57e4e567d2e69eda6c4e7
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-27 14:36:26 +0100

    adding onSubmit and onChange to Form

--- Calculateur/src/components/Form/Form.tsx
+++ Calculateur/src/components/Form/Form.tsx
@@ -1,8 +1,24 @@
+import { FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addNewOperation } from '../../app/operationsSlice'
+
 function Form() {
+    const [name, setName] = useState('')
+    const [amount, setAmount] = useState(0)
+    const dispatch = useDispatch()
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        const newOperation = {
+            name: name,
+            amount: amount,
+        }
+        dispatch(addNewOperation(newOperation))
+    }
     return (
         <>
             <h2>Add a new operation</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">name</label>
                     <input
@@ -10,6 +26,10 @@ function Form() {
                         id="name"
                         name="name"
                         className="form-control"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.currentTarget.value)
+                        }}
                     />
                 </div>
                 <div className="form-group">
@@ -19,7 +39,10 @@ function Form() {
                         id="amount"
                         name="amount"
                         className="form-control"
-                        placeholder="0"
+                        value={amount}
+                        onChange={(e) => {
+                            setAmount(+e.currentTarget.value)
+                        }}
                     />
                 </div>
                 <div className="form-group d-flex justify-content-end my-3">
