1bae8ba46551d0d5269d6f1fefd32451df228456
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-27 12:39:52 +0100

    Calculator with style

--- Calculateur/src/Calculator.tsx
+++ Calculateur/src/Calculator.tsx
@@ -6,12 +6,28 @@ import Detail from './components/Detail/Detail'
 
 function Calculator() {
     return (
-        <>
-            <Total />
-            <Synthesis />
-            <Detail />
-            <Form />
-        </>
+        <div className="container-fluid bg-light template">
+            <header className="row">
+                <div className="col-6 offset-3">
+                    <Total />
+                </div>
+            </header>
+            <section className="row">
+                <div className="col-6 offset-3">
+                    <Synthesis />
+                </div>
+            </section>
+            <main className="row">
+                <div className="col-6 offset-3">
+                    <Detail />
+                </div>
+            </main>
+            <footer className="row">
+                <div className="col-6 offset-3">
+                    <Form />
+                </div>
+            </footer>
+        </div>
     )
 }
 
