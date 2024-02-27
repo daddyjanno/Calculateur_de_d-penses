6b32b99282e1de979de009a7042b9e000dac4090
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-27 12:19:40 +0100

    main

--- Calculateur/src/main.tsx
+++ Calculateur/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Calculateur from './Calculateur.tsx'
+import Calculator from './Calculator.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Calculateur />
+        <Calculator />
     </React.StrictMode>
 )
