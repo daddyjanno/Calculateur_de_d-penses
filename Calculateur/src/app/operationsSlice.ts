13659c63608341343b6a76d2f311feb34d6ce0d8
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-27 14:36:50 +0100

    operationSlice

---
+++ Calculateur/src/app/operationsSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { createMockedData } from '../utils/createMockedData'
+
+interface IProps {
+    id: number
+    name: string
+    amount: number
+}
+const mockedData: IProps[] = createMockedData()
+const initialState = mockedData
+
+const operationSlice = createSlice({
+    name: 'operations',
+    initialState,
+    reducers: {
+        addNewOperation(state, action) {
+            state.unshift(action.payload)
+        },
+        deleteOperation(state, action) {
+            state.filter((item) => {
+                return item.id !== action.payload
+            })
+        },
+        updateOperation(state, action) {
+            return state.map((operation) => {
+                if (operation.id === action.payload.id) {
+                    return {
+                        id: action.payload.id,
+                        name: action.payload.name,
+                        amount: action.payload.amount,
+                    }
+                } else {
+                    return operation
+                }
+            })
+        },
+    },
+})
+
+// selectors
+
+export const getOperations = (state: { operations: Array<object> }) =>
+    state.operations
+
+// actions
+
+export const { addNewOperation, deleteOperation, updateOperation } =
+    operationSlice.actions
+
+// reducers
+
+export const operationsReducer = operationSlice.reducer
