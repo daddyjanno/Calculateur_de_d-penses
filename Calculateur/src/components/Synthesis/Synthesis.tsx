423e4c5c8fd9b09ad9cb053d58a26aa2ca80b77f
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-27 12:20:06 +0100

    Synthesis component

---
+++ Calculateur/src/components/Synthesis/Synthesis.tsx
@@ -0,0 +1,17 @@
+function Synthesis() {
+    return (
+        <div className="text-center">
+            <h2>Synthesis</h2>
+            <div className="row">
+                <div className="col-6">
+                    <h3 className="fs-4">Income</h3>
+                </div>
+                <div className="col-6">
+                    <h3 className="fs-4">Expenses</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Synthesis
