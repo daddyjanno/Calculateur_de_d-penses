c2a4f736cc3295fc91417e388712b95832af249e
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-28 13:22:53 +0100

    Synthesis now display income and expense total

--- Calculateur/src/components/Synthesis/Synthesis.tsx
+++ Calculateur/src/components/Synthesis/Synthesis.tsx
@@ -1,13 +1,39 @@
-function Synthesis() {
+interface IProps {
+    id: number
+    name: string
+    amount: number
+}
+
+function Synthesis({ operations }: { operations: Array<IProps> }) {
+    console.log(operations)
+
     return (
         <div className="text-center">
             <h2>Synthesis</h2>
             <div className="row">
                 <div className="col-6">
                     <h3 className="fs-4">Income</h3>
+                    <p className="fs-5">
+                        {operations
+                            .filter((item) => {
+                                return item.amount >= 0
+                            })
+                            .reduce((acc, item) => {
+                                return acc + item.amount
+                            }, 0)}
+                    </p>
                 </div>
                 <div className="col-6">
                     <h3 className="fs-4">Expenses</h3>
+                    <p className="fs-5">
+                        {operations
+                            .filter((item) => {
+                                return item.amount < 0
+                            })
+                            .reduce((acc, item) => {
+                                return acc + item.amount
+                            }, 0)}
+                    </p>
                 </div>
             </div>
             <br />
