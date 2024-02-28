93f1e530fa67b56e7063c2c2cf843dc53e9d7089
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-28 13:21:46 +0100

    Calculator

--- Calculateur/src/Calculator.tsx
+++ Calculateur/src/Calculator.tsx
@@ -13,12 +13,12 @@ function Calculator() {
         <div className="container-xl bg-light template">
             <header className="row">
                 <div className="col-6 offset-3">
-                    <Total />
+                    <Total operations={operations} />
                 </div>
             </header>
             <section className="row">
                 <div className="col-6 offset-3">
-                    <Synthesis />
+                    <Synthesis operations={operations} />
                 </div>
             </section>
             <main className="row">
