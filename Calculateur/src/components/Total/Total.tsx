08de418530c18019f0f8f26e9b1c2c20a6a57c13
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-28 13:22:58 +0100

    Total

--- Calculateur/src/components/Total/Total.tsx
+++ Calculateur/src/components/Total/Total.tsx
@@ -1,7 +1,21 @@
-function Total() {
+import { ReactNode } from 'react'
+
+interface IProps {
+    id: number
+    name: string
+    amount: number
+}
+
+function Total({ operations }: { operations: Array<IProps> }): ReactNode {
     return (
         <div className="text-center">
             <h1>Total</h1>
+            <p className="fs-3">
+                {operations.reduce((accumulator, currentValue) => {
+                    return accumulator + currentValue.amount
+                }, 0)}{' '}
+                €
+            </p>
             <br />
         </div>
     )
